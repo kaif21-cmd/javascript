12fff5ecbacb5911fbfe63306fcae12f4003d629
Kaif Shaikh Siddique <85302180+kaif21-cmd@users.noreply.github.com>
2023-03-21 01:43:14 +0700

    Add files via upload

---
+++ Arithmetic operation.js
@@ -0,0 +1,20 @@
+console.log('arithmetic operations kaif shaikh')
+let x=5;
+let y=10;
+let z=x+y; //+operator
+let z1=x-y; //-operator
+let z2=x%y; //%operator
+let z3=x*y; //*operator
+let z4=x/y; // /operator
+
+console.log(z);
+console.log(z1);
+console.log(z2);
+console.log(z3);
+console.log(z4);
+
+//others operator are
+//1.Incremnet and decrement operator ++ and --//
+
+
+
