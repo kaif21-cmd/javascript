86a72f001e9a9720290f5abbe2989ed8a8d3511d
Kaif Shaikh Siddique <85302180+kaif21-cmd@users.noreply.github.com>
2023-03-21 11:21:59 +0700

    Add files via upload

---
+++ Data Types.js
@@ -0,0 +1,37 @@
+console.log('java script data types kaif shaikh ')
+//1.string
+//2.Number
+//3.Bingint
+//4.Boolean
+//5.Undefined
+//6.null
+//7.symbol
+//8.object
+//object data type are-- 1.array,2.abject,3.date;
+//------------//
+//1.number
+let l=16;
+let w=7.5;
+let h=l*w;
+console.log( 'height=length*wridth='+h)
+//2.string
+let color="yellow";
+let lastname="kaif shaikh";
+console.log(color + lastname)
+//3.Bollean
+let x=5;
+let y=5;
+let z=6;
+console.log((x==y)+''+(x==z));
+//4.Object
+const person ={
+    firstName:"Kaif",lastName:"shaikh"};
+   console.log(person.firstName+person.lastName)
+//5.Array object
+const car=["Bmw","Audi","Mercides"];
+console.log(car[0],car[1]);
+//6.
+let j= BigInt("48392872922082220");
+console.log(j);
+
+
