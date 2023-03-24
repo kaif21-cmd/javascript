4776985d647f7b727e1b2df07a4fae8f6e1c7292
Kaif Shaikh Siddique <85302180+kaif21-cmd@users.noreply.github.com>
2023-03-24 13:15:58 +0700

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,28 @@
+console.log('kaif shaikh')
+const car=["sab","volvo","BMW"];
+console.log(car[0])
+console.log(car[2])
+//Using the JavaScript Keyword new
+//The following example also creates an Array, and assigns values to it:
+const car1=new Array("audi","Mercedes")
+console.log(car1[1])
+//Changing an Array Element
+//This statement changes the value of the first element in cars:
+car1[0]="kaif";
+console.log(car1[0])
+//acces full array
+console.log(car)
+//create person and acces elemet
+const person=["kaif","shaikh",21]
+console.log(person[0],person[2])
+//the lenght property of array
+const fruits=["banana","orange","apple","mango"];
+console.log(fruits.length)
+//adding array element
+const fruits1=["banana","orange","Apple"];
+console.log(fruits1)
+function myFunction(){
+    fruits[fruits1.length]="lemon";
+}
+console.log(fruits1)
+
